745f56441863336008893c01f90c42dc0db291ab
John Arthur <thorjarhun@gmail.com>
2016-11-03 15:54:44 -0500

    update parties

--- examples/other/parties/client/client.js
+++ examples/other/parties/client/client.js
@@ -113,7 +113,7 @@ var coordsRelativeToElement = function (element, event) {
   return { x: x, y: y };
 };
 
-Template.map.events({
+Template.my_map.events({
   'mousedown circle, mousedown text': function (event, template) {
     Session.set("selected", event.currentTarget.id);
   },
@@ -125,7 +125,7 @@ Template.map.events({
   }
 });
 
-Template.map.onRendered(function () {
+Template.my_map.rendered = function () {
   var self = this;
   self.node = self.find("svg");
 
@@ -140,19 +140,19 @@ Template.map.onRendered(function () {
       // Draw a circle for each party
       var updateCircles = function (group) {
         group.attr("id", function (party) { return party._id; })
-        .attr("cx", function (party) { return party.x * 500; })
-        .attr("cy", function (party) { return party.y * 500; })
-        .attr("r", radius)
-        .attr("class", function (party) {
-          return party.public ? "public" : "private";
-        })
-        .style('opacity', function (party) {
-          return selected === party._id ? 1 : 0.6;
-        });
+            .attr("cx", function (party) { return party.x * 500; })
+            .attr("cy", function (party) { return party.y * 500; })
+            .attr("r", radius)
+            .attr("class", function (party) {
+              return party.public ? "public" : "private";
+            })
+            .style('opacity', function (party) {
+              return selected === party._id ? 1 : 0.6;
+            });
       };
 
       var circles = d3.select(self.node).select(".circles").selectAll("circle")
-        .data(Parties.find().fetch(), function (party) { return party._id; });
+          .data(Parties.find().fetch(), function (party) { return party._id; });
 
       updateCircles(circles.enter().append("circle"));
       updateCircles(circles.transition().duration(250).ease("cubic-out"));
@@ -161,16 +161,16 @@ Template.map.onRendered(function () {
       // Label each with the current attendance count
       var updateLabels = function (group) {
         group.attr("id", function (party) { return party._id; })
-        .text(function (party) {return attending(party) || '';})
-        .attr("x", function (party) { return party.x * 500; })
-        .attr("y", function (party) { return party.y * 500 + radius(party)/2 })
-        .style('font-size', function (party) {
-          return radius(party) * 1.25 + "px";
-        });
+            .text(function (party) {return attending(party) || '';})
+            .attr("x", function (party) { return party.x * 500; })
+            .attr("y", function (party) { return party.y * 500 + radius(party)/2 })
+            .style('font-size', function (party) {
+              return radius(party) * 1.25 + "px";
+            });
       };
 
       var labels = d3.select(self.node).select(".labels").selectAll("text")
-        .data(Parties.find().fetch(), function (party) { return party._id; });
+          .data(Parties.find().fetch(), function (party) { return party._id; });
 
       updateLabels(labels.enter().append("text"));
       updateLabels(labels.transition().duration(250).ease("cubic-out"));
@@ -178,20 +178,20 @@ Template.map.onRendered(function () {
 
       // Draw a dashed circle around the currently selected party, if any
       var callout = d3.select(self.node).select("circle.callout")
-        .transition().duration(250).ease("cubic-out");
+          .transition().duration(250).ease("cubic-out");
       if (selectedParty)
         callout.attr("cx", selectedParty.x * 500)
-        .attr("cy", selectedParty.y * 500)
-        .attr("r", radius(selectedParty) + 10)
-        .attr("class", "callout")
-        .attr("display", '');
+            .attr("cy", selectedParty.y * 500)
+            .attr("r", radius(selectedParty) + 10)
+            .attr("class", "callout")
+            .attr("display", '');
       else
         callout.attr("display", 'none');
     });
   }
-});
+};
 
-Template.map.onDestroyed = function () {
+Template.my_map.onDestroyed = function () {
   this.handle && this.handle.stop();
 };
 
